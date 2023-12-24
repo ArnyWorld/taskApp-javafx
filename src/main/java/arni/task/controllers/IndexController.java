47f56028b7fc2881e2b4c84e7d6cf2b7f9a809bb
Arnaldo Muñoz <kings31lendy@gmail.com>
2023-12-23 23:40:53 -0400

    add method POST, PUT and DELETE

--- src/main/java/arni/task/controllers/IndexController.java
+++ src/main/java/arni/task/controllers/IndexController.java
@@ -35,6 +35,14 @@ public class IndexController implements Initializable {
     private TableColumn<Task, Integer> statusColumnTask;
 
     private final ObservableList<Task> taskList = FXCollections.observableArrayList();
+    @FXML
+    private TextField textTask;
+    @FXML
+    private TextField textManager;
+    @FXML
+    private TextField textStatus;
+
+    private Integer idTask;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -56,4 +64,89 @@ public class IndexController implements Initializable {
         taskList.addAll(taskService.listTasks());
         taskTable.setItems(taskList);
     }
+
+    public void addTask(){
+        if(textTask.getText().isEmpty()){
+            showMessage("Error Validation", "task name is required");
+            textTask.requestFocus();
+            return;
+        }else{
+            var task = new Task();
+            getDataForm(task);
+            task.setIdTask(null);
+            taskService.saveTask(task);
+            showMessage("Information", "Task Saved");
+            clearForm();
+            listTasks();
+
+        }
+    }
+
+    public void loadFormTask(){
+        var task = taskTable.getSelectionModel().getSelectedItem();
+        if(task!=null){
+            idTask = task.getIdTask();
+            textTask.setText(task.getNameTask());
+            textManager.setText(task.getManager());
+            textStatus.setText(task.getStatus());
+        }
+    }
+
+    private void getDataForm(Task task){
+        if(idTask!=null) {
+            task.setIdTask(idTask);
+            task.setNameTask(textTask.getText());
+            task.setManager(textManager.getText());
+            task.setStatus(textStatus.getText());
+        }
+    }
+
+    private void showMessage(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle((title));
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public void modifyTask(){
+        if(idTask==null){
+            showMessage("Information", "select a task");
+            return;
+        }
+        if(textTask.getText().isEmpty()){
+            showMessage("Error Validation", "Select a task");
+            textTask.requestFocus();
+            return;
+        }
+        var task = new Task();
+        getDataForm(task);
+        taskService.saveTask(task);
+        showMessage("Information", "Task edited");
+        clearForm();
+        listTasks();
+
+    }
+
+    public void deleteTask(){
+        var task = taskTable.getSelectionModel().getSelectedItem();
+        if(task!=null){
+            logger.info("delete Register"+task.toString());
+            taskService.deleteTask(task.getIdTask());
+            showMessage("Information", "Deleted task"+task.getIdTask());
+            clearForm();
+            listTasks();
+        }
+        else{
+            showMessage("Error", "Select a row");
+        }
+    }
+    public void clearForm(){
+        idTask = null;
+        textTask.clear();
+        textManager.clear();
+        textStatus.clear();
+
+    }
+
 }
